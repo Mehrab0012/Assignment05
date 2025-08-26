6fa7c3cd179b4e3bd7157ecda22e614a803d93cb
Mehrab0012 <mehrabhossainfr@gmail.com>
2025-08-26 23:36:46 +0600

    Adding a

--- home.js
+++ home.js
@@ -23,6 +23,30 @@ heartsButton.forEach(heart=>{
 })
 });
 
-// Coin Counter
+// Copy Button
+const copyButton = classesCatcher(".copy-button");
+const copies = idCatcher("copies");
 
+copyButton.forEach(copy=>{
+    copy.addEventListener('click', ()=>{
+        const copyCountNumber =parseInt(copies.innerText);
+        idCatcher("copies").innerText=copyCountNumber+1;
+        navigator.clipboard.writeText(copy.value);
 
+    })
+})
+
+//Coin counter
+const callButton = classesCatcher(".call-button");
+const coins = idCatcher("coins");
+
+callButton.forEach(call=>{
+    call.addEventListener('click', ()=>{
+        const coinNumbers= parseInt(coins.innerText);
+        if(coinNumbers<20){
+            alert("INSUFFICIENT COINS");
+            return "";
+        }
+        idCatcher("coins").innerText = coinNumbers-20;
+    })
+})
